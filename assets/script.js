4aed4cfb58e40201837cf5aa74a28fc71cd1bcd8
Audria <audriaisforme@gmail.com>
2024-03-26 09:01:45 -0500

    Deleted duplicate city search

--- assets/script.js
+++ assets/script.js
@@ -47,14 +47,12 @@ axios.get(apiUrl).then(refreshWeather);}
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector('#search-form-input');
-  
-  let cityElement = document.querySelector('#city');
-  cityElement.innerHTML = searchInput.value;
+  let searchInput = document.querySelector("#search-form-input");
+
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector('#search-form');
-searchFormElement.addEventListener('submit', handleSearchSubmit);
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity('Paris');
+searchCity("Paris");
